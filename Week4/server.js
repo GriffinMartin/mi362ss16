c08a26e87506c58508d201529206d46eac226082
GriffinMartin <gmart248@gmail.com>
2016-02-11 17:08:08 -0500

    node html

---
+++ Week4/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer( (request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('<h1>Hello World</h1>\n');
+  response.end('<p>This is a node app!</p>\n');
+}).listen(8124);
+
+console.log('Server running at http://127.0.0.1:8124/');
